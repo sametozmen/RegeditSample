13dd18a45847a971df3cf5e8a4b5fae793389462
sametozmen <53569643+sametozmen@users.noreply.github.com>
2019-10-09 23:51:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using System.Threading.Tasks;
+
+namespace RegeditSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            returnValue();
+        }
+
+
+
+
+        public static void returnValue()
+        {
+            Registry.Users.GetSubKeyNames().Select(x => Registry.Users.OpenSubKey(x))
+            .Traverse(key =>
+            {
+                if (key != null)
+                {
+                    return key.GetSubKeyNames().Select(subKey => key.OpenSubKey(subKey));
+                }
+
+                return null;
+            })
+            .ForEach(key =>
+            {
+                key.GetValueNames().ForEach(valueName => Console.WriteLine("{0}\\{1}:{2} ({3})", key, valueName, key.GetValue(valueName), key.GetValueKind(valueName)));
+            });
+
+            Console.ReadLine();
+        }
+    }
+    public static class Extensions
+    {
+        public static IEnumerable<T> Traverse<T>(this IEnumerable<T> source, Func<T, IEnumerable<T>> fnRecurse)
+        {
+            foreach (T item in source)
+            {
+                yield return item;
+
+                IEnumerable<T> seqRecurse = fnRecurse(item);
+
+                if (seqRecurse != null)
+                {
+                    foreach (T itemRecurse in Traverse(seqRecurse, fnRecurse))
+                    {
+                        yield return itemRecurse;
+                    }
+                }
+            }
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+            {
+                action(item);
+            }
+
+
+
+
+        }
+    }
+}
---
+++ regeditusers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace RegeditSample
+{
+    class regeditusers
+    {
+        public void goster()
+        {
+            //ArrayList a = new ArrayList();
+            //foreach (object item in Registry.Users.GetSubKeyNames())
+            //{
+            //    a.Add(item);
+            //}
+            //foreach (string item in Registry.Users.OpenSubKey(a).ToString())
+            //{
+            //    a.Add(item);
+            //}
+            //Console.WriteLine(Registry.ClassesRoot.ToString());
+            //foreach (object eleman1 in a)
+            //    Console.WriteLine(eleman1);
+            //Console.WriteLine(eleman1);
+            //foreach (object eleman2 in Registry.ClassesRoot.GetSubKeyNames())
+            //    Console.WriteLine(eleman2);     
+        }
+    }
+}
+
